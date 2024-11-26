1bb85899c7ffaf863a0daf0873a4e57ac152498f
Berme06 <hectorchachoportugal@gmail.com>
2024-11-26 13:53:03 +0100

    HTML

---
+++ Javascript/tt.js
@@ -0,0 +1,22 @@
+console.log('pagina cargada');
+const NUM_ALUMNOS = 17
+  
+function ImprimirMensaje(mensaje){
+    console.log(mensaje);
+    //alert(mensaje);
+    console.log("Hola,mundo");
+}
+function tipoDeVariable(mivariable){
+    let mensaje = ""Mi vaariable es de tipo" + typeof(miVariable)"
+    console.log(mensaje)
+    console.log(NUM_ALUMNOS)
+}
+
+var alumnoFila2= ["Jacob", "Aleks", "David"; "Jorge"];
+console.log(alumnosFila2);
+
+var notaExamenJs= 6;
+console.log(typeof(notaExamenJs));
+
+var miNombre = "Berme";
+tipoDeVariable(miNombre);
